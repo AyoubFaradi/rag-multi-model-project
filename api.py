49dfc3383669ec3117ea040ea75097cfb8444da0
Ayoub Faradi <162796702+AyoubFaradi@users.noreply.github.com>
2025-12-31 01:09:08 +0100

    modification sur data l'ajoute de pdf mundiapolis

--- api.py
+++ api.py
@@ -16,7 +16,6 @@ async def rag_chat(request: Request):
     body = await request.json()
     question = body.get("question", "")
 
-    # 🔥 VRAI RAG (PDF + pgvector)
     resp_text, _ = answer(question, k=5)
 
     return {"answer": resp_text}
